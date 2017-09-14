d3684e8ef3c61b290cf4af421dae4ce4db4a6052
Daniel Ceballos <daniel@cactus.fm>
2017-09-13 22:12:03 -0700

    data structure v1

--- public/main.js
+++ public/main.js
@@ -4,25 +4,26 @@ window.onload=function(){
   animate();
   var loader = new THREE.OBJLoader();
   loader.load( 'cactus-3d.obj', function ( object ) {
-    scene.add(object);
 
+    scene.add(object);
     var verticalScale = 1.9;
     object.children.forEach(function(mesh) {
       console.log(mesh.name);
+			//var material = new THREE.MeshLambertMaterial({reflectivity:1});
+			//mesh.material = material;
       mesh.geometry.dynamic = true;
       mesh.geometry.verticesNeedUpdate = true;
-      //var mat = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe : false  }  );
-      //mesh.material = mat;
     });
 
-    var head   = object.getObjectByName("Layer_Horizontal_Frame_1_Head_ESEL101A");
-    var sill   = object.getObjectByName("Layer_Horizontal_Frame_2_Sill_ESEL102");
-    var jamb1  = object.getObjectByName("Layer_Vertical_Frame_1_Jamb_1_ESEL104");
-    var jamb2  = object.getObjectByName("Layer_Vertical_Frame_2_Jamb_2_ESEL104");
-    var hbead1  = object.getObjectByName("Layer_Horizontal_A_1_Glazing_Bead_Fixed__1_ESEL110");
-    var hbead2  = object.getObjectByName("Layer_Horizontal_A_2_Glazing_Bead_Fixed_2_ESEL110");
-    var vbead1 = object.getObjectByName("Layer_Vertical_A_1_Glazing_Bead_Fixed_1_ESEL110");
-    var vbead2 = object.getObjectByName("Layer_Vertical_A_2_Glazing_Bead_Fixed_2_ESEL110");
+
+    var head   = object.getObjectByName("Layer_Frame_Top_1_Head_ESEL101A");
+    var sill   = object.getObjectByName("Layer_Frame_Bottom_1_Sill_ESEL102");
+    var jamb1  = object.getObjectByName("Layer_Frame_Left_1_Jamb_1_ESEL104");
+    var jamb2  = object.getObjectByName("Layer_Frame_Right_1_Jamb_2_ESEL104");
+    var hbead1 = object.getObjectByName("Layer_A_Top_1_Glazing_Bead_Fixed__1_ESEL110");
+    var hbead2 = object.getObjectByName("Layer_A_Bottom_1_Glazing_Bead_Fixed_2_ESEL110");
+    var vbead1 = object.getObjectByName("Layer_A_Left_1_Glazing_Bead_Fixed_1_ESEL110");
+    var vbead2 = object.getObjectByName("Layer_A_Right_1_Glazing_Bead_Fixed_2_ESEL110");
 
     var jambBox       = new THREE.Box3().setFromObject(jamb1);
     var jambHeight    = jambBox.max.y - jambBox.min.y;
@@ -64,7 +65,48 @@ window.onload=function(){
     //jamb1.position.x = v1.distanceTo(v2)+10;
     //bead1.position.z = -headOffset;
     //vbead1.position.z = 1;
+		console.log(meshesAsParts(object));
   });
+}
+
+function meshesAsParts(object) {
+	// ie: Layer_A_Top_1_Glazing_Bead_Fixed__1_ESEL110
+	var parts = {frame:{}, panels:{}, rails:{}};
+	object.children.forEach(function(mesh) {
+		var meshParts = mesh.name.split("_").map(function(m) {
+			return m.toLowerCase();	
+		});
+		var name = meshParts[1];
+		if (name == "frame") {
+			var position = meshParts[2];
+			var stackPos = meshParts[3];
+			if (parts[name][position]) {
+				parts[name][position][stackPos] = mesh;
+			}else{
+				parts[name][position] = {};
+				parts[name][position][stackPos] = mesh;
+			}
+		}else if (name == "rail") {
+			var position = meshParts[2];
+			parts['rails'][position] = mesh;
+		}else if (name.length == 1) {
+			var position = meshParts[2];
+			var stackPos = meshParts[3];
+			if (parts['panels'][name]) {
+				if (parts['panels'][name][position]) {
+					parts['panels'][name][position][stackPos] = mesh;
+				}else{
+					parts['panels'][name][position] = {};
+					parts['panels'][name][position][stackPos] = mesh;
+				}
+			}else{
+				parts['panels'][name] = {};
+				parts['panels'][name][position] = {};
+				parts['panels'][name][position][stackPos] = mesh;
+			}
+		}
+	});
+	return parts;
 }
 
 window.addEventListener( 'resize', resize, false );
